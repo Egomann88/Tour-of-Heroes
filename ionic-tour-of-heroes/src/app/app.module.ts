98e4ca9d85ee4e78d88c1ec86ab704dd49e9a863
Justin <justin-urbanek@bluewin.ch>
2024-01-16 09:08:22 +0100

    chore: add explanations to app.module imports

--- ionic-tour-of-heroes/src/app/app.module.ts
+++ ionic-tour-of-heroes/src/app/app.module.ts
@@ -10,13 +10,17 @@ import { AppComponent } from './app.component';
 import { HeroManageComponent } from './hero-manage/hero-manage.component';
 
 @NgModule({
+  // only declare components, directives and pipes
   declarations: [AppComponent, HeroManageComponent],
   imports: [
+    // only import modules
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
     FormsModule,
   ],
+  // only export modules
+  exports: [],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
