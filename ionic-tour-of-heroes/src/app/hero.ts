8791ac917e94681f8c43568e738ae45a77eb38fe
Justin <justin-urbanek@bluewin.ch>
2024-01-16 09:42:45 +0100

    feat:refactor: add superPowers, interface to class

--- ionic-tour-of-heroes/src/app/hero.ts
+++ ionic-tour-of-heroes/src/app/hero.ts
@@ -1,4 +1,11 @@
-export interface Hero {
+export class Hero {
   id: number;
   name: string;
+  superPower?: string;
+
+  constructor(id: number, name: string, superPower?: string) {
+    this.id = id;
+    this.name = name;
+    this.superPower = superPower;
+  }
 }
