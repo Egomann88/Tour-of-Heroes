10cd5587fed5f2865e2338714e61d70d67174218
Justin <justin-urbanek@bluewin.ch>
2024-01-17 15:04:14 +0100

    refactor: remove explore-container

--- ionic-tour-of-heroes/src/app/heroes/heroes.module.ts
+++ ionic-tour-of-heroes/src/app/heroes/heroes.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HeroesPage } from './heroes.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { HeroesPageRoutingModule } from './heroes-routing.module';
 
@@ -12,7 +11,6 @@ import { HeroesPageRoutingModule } from './heroes-routing.module';
     IonicModule,
     CommonModule,
     FormsModule,
-    ExploreContainerComponentModule,
     HeroesPageRoutingModule,
   ],
   declarations: [HeroesPage],
--- ionic-tour-of-heroes/src/app/settings/settings.module.ts
+++ ionic-tour-of-heroes/src/app/settings/settings.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { settingsPage } from './settings.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { settingsPageRoutingModule } from './settings-routing.module';
 
@@ -12,7 +11,6 @@ import { settingsPageRoutingModule } from './settings-routing.module';
     IonicModule,
     CommonModule,
     FormsModule,
-    ExploreContainerComponentModule,
     settingsPageRoutingModule,
   ],
   declarations: [settingsPage],
