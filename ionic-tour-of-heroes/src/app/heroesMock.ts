83397795915b934426cf320bb328b65f4d4fb803
Justin <justin-urbanek@bluewin.ch>
2024-01-17 14:20:28 +0100

    refactor: Mock & Service refactored

--- ionic-tour-of-heroes/src/app/heroesMock.ts
+++ ionic-tour-of-heroes/src/app/heroesMock.ts
@@ -1,13 +1,13 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  new Hero(12, 'Dr Nice'),
-  new Hero(13, 'Bombasto', 'Super Hot'),
-  new Hero(14, 'Celeritas'),
-  new Hero(15, 'Magneta'),
-  new Hero(16, 'RubberMan', 'Super Flexible'),
-  new Hero(17, 'Dynama'),
-  new Hero(18, 'Dr IQ', 'Really Smart'),
-  new Hero(19, 'Magma'),
-  new Hero(20, 'Tornado', 'Weather Changer'),
+  new Hero('Dr Nice'),
+  new Hero('Bombasto', 'Super Hot'),
+  new Hero('Celeritas'),
+  new Hero('Magneta'),
+  new Hero('RubberMan', 'Super Flexible'),
+  new Hero('Dynama'),
+  new Hero('Dr IQ', 'Really Smart'),
+  new Hero('Magma'),
+  new Hero('Tornado', 'Weather Changer'),
 ];
